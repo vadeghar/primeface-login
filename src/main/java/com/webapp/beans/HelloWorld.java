00856767fa7385d2333bafcd853181fdca18511d
Lakshman Vadeghar <vadeghar@gmail.com>
2019-05-23 00:29:33 +0530

    Initial Code

---
+++ src/main/java/com/webapp/beans/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.webapp.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloWorld {
+
+	private String firstName = "John";
+	private String lastName = "Doe";
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGreeting() {
+		return "Hello " + firstName + " " + lastName + "!";
+	}
+}
